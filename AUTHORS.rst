675c1f22a9503b8b53f70bf36afd0473d19356a7
Pavel Trakhtman <pavel@alertlogic.com>
2020-02-06 14:12:44 -0600

    Publish alpha to pypi

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Alert Logic, Inc. <devsupport@alertlogic.com>
+
+Contributors
+------------
+
+None yet. Why not be the first?
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import re
 import ast
+from os import path
 from setuptools import setup, find_packages
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -8,20 +9,36 @@ with open('almdrlib/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='alertlogic-sdk-python',
     version=version,
     url='https://github.com/alertlogic/alertlogic-sdk-python',
     license='MIT',
     author='Alert Logic Inc.',
-    author_email='support@alertlogic.com',
+    author_email='devsupport@alertlogic.com',
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
     description='Alert Logic Software Development Kit for Python.',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     scripts=[],
     packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'troubleshooting']),
     include_package_data=True,
     zip_safe=False,
     platforms='any',
-    python_requires='>=3.7',
     install_requires=[
         'requests>=2.18',
         'configparser>=4.0.2',
