6ee6fb3a431e7b320e7f0faef918d5c6de82f3e4
Pavel Trakhtman <pavel@alertlogic.com>
2020-03-06 08:34:49 -0600

    Removed incorrect reference to AlertLogic_Logo_White.png

--- docs/conf.py
+++ docs/conf.py
@@ -107,12 +107,9 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = "AlertLogic_Logo_White.png"
-
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 
-# html_style = 'css/custom.css'
 html_css_files = [
     'css/custom.css'
 ]
