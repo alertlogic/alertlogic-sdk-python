e3edda82b765a8061351d33f4f1fdcc73a2bf699
EvanBrown96 <evan.brown@alertlogic.com>
2023-02-22 14:38:31 -0500

    add tests for new ssm functionality

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -30,10 +30,38 @@ class MockBotoDDB:
     def __init__(self, client_type):
         pass
 
+class SSMExceptions:
+
+    class ParameterNotFound(Exception):
+        pass
+
+class MockBotoSSM:
+
+    exceptions = SSMExceptions()
+
+    def __init__(self, client_type):
+        pass
+
+    @staticmethod
+    def get_parameter(**kwargs):
+        Name = kwargs.get('Name')
+        WithDecryption = kwargs.get('WithDecryption')
+
+        if Name == '/deployments/us-west-1/env-settings/someapplication.stringparam':
+            return {'Parameter': {'Value': 'testingtesting'}}
+        elif Name == '/deployments/us-west-1/env-settings/someapplication.securestringparam':
+            if WithDecryption:
+                return {'Parameter': {'Value': 'testingtestingtesting'}}
+            else:
+                return {'Parameter': {'Value': 'AQICAHhxjBnL9Dhpl/8CUT7/NC07HUYnX+P'}}
+        else:
+            raise MockBotoSSM.exceptions.ParameterNotFound()
+
 
 class TestAlEnv(unittest.TestCase):
     def test_something(self):
         boto3.resource = MagicMock(return_value=MockBotoDDB)
+        boto3.client = MagicMock(return_value=MockBotoSSM)
         os.environ['ALERTLOGIC_STACK_REGION'] = 'us-west-1'
         os.environ['ALERTLOGIC_STACK_NAME'] = 'production'
         env = AlEnv("someapplication")
@@ -47,7 +75,11 @@ class TestAlEnv(unittest.TestCase):
         assert env.get("boolkeytrue", type='boolean') is True
         assert env.get("floatkey") == '1.0'
         assert env.get("floatkey", type='float') == 1.0
-
+        assert env.get_parameter("stringparam") == 'testingtesting'
+        assert env.get_parameter("securestringparam") == 'AQICAHhxjBnL9Dhpl/8CUT7/NC07HUYnX+P'
+        assert env.get_parameter("securestringparam", decrypt=False) == 'AQICAHhxjBnL9Dhpl/8CUT7/NC07HUYnX+P'
+        assert env.get_parameter("securestringparam", decrypt=True) == 'testingtestingtesting'
+        assert env.get_parameter("invalidparam", "default") == 'default'
 
 if __name__ == '__main__':
     unittest.main()
